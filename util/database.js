0f2b8371b1eb743778247d36473d3b47d0b8907a
Aayush <88710500+kaayush163@users.noreply.github.com>
2023-08-21 13:56:47 +0530

    change database.js

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('todolist','root','nodemysql',    // here booking you have to manually create on mysql work bench password put in''
+const sequelize = new Sequelize('todolist','root','nodemysql',   
 {
     dialect : 'mysql',
     host : 'localhost'
