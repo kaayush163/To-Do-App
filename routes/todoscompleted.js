1f2f157974b4dab3819fc24eff70bbadffd0b3ed
Aayush <88710500+kaayush163@users.noreply.github.com>
2023-08-21 13:49:10 +0530

    Update todoscompleted.js

--- routes/todoscompleted.js
+++ routes/todoscompleted.js
@@ -5,7 +5,6 @@ const userAuthenticate=require('../middleware/auth');
 
 router.get('/get-completed',userAuthenticate.authenticate, completed.getCompleted);
 router.post('/post-completed', userAuthenticate.authenticate,completed.postCompleted);
-router.post('/edit-completed/:completedId',userAuthenticate.authenticate, completed.editCompleted);
 router.get('/delete-completed/:completedId', userAuthenticate.authenticate,completed.deleteCompleted);
 
 module.exports = router;
