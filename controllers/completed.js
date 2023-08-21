3e1f2889f930fc3ac42537355257dbdbfd34a840
Aayush <88710500+kaayush163@users.noreply.github.com>
2023-08-21 13:55:18 +0530

    Update completed.js

--- controllers/completed.js
+++ controllers/completed.js
@@ -15,14 +15,11 @@ exports.getCompleted = async (req, res, next) => {
 exports.postCompleted = async (req, res, next) => {
     try {
       const text = req.body.text;
-      // const email = req.body.email;
-      // const mobile = req.body.mobile;
       const data = await CompletedTodo.create({
         text:text,
         userId:req.user.id
       });
       res.status(201).json(data);
-      // res.status(201).json({ newUserDetail: data });
     } catch (err) {
       res.status(404).json({ error: err });
     }
